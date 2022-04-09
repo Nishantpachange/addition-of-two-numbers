7a41468a0131ef6024ff907fb87c723dbf286f6f
Nishant pachange <103159744+Nishantpachange@users.noreply.github.com>
2022-04-09 12:10:48 +0530

    Add files via upload

---
+++ raj.c
@@ -0,0 +1,36 @@
+
+
+#include<stdio.h>
+using namespace std;
+void main()
+{
+	int a =50,b=60,c=70,d=80,e=90,f=100;
+	if(a<b)
+	{
+		printf("grade f+",a);
+	
+		if(b<c)
+		{
+			printf("grade e+",b);
+		}
+		else(c<d)
+		{
+			printf("grade g",c);
+			
+		}
+		
+		
+	}
+if (d<e);
+{
+	printf("grade h",d);
+	
+}
+else(e<f)
+{
+	printf("grade j",i)
+}
+	}	
+
+
+
